ae8e0d55b39216486cb9b2b462294a01d585b6e0
USER <USER@MSI>
2019-10-04 17:31:47 +0800

    說明修改

--- Designpattern/src/solid/dip/Main.java
+++ Designpattern/src/solid/dip/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	/*	依賴倒置原則 (Dependency-Inversion Principle, DIP)
 	 * 
+	 * 	所有物件的相依關係需要依賴於抽象類別
 	 * 	目的是為了 解除高階模組 (People) 與低階模組 (Hamburger) 的耦合關係
 	 * 	People 不再依賴 Hamburger ，而是兩者都依賴 Stuffer 介面
 	 * 	=> 高階模組不應該依賴於低階模組，兩者都該依賴抽象
