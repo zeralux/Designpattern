01881483109c4bed8df89c5e0b83888ac9f61136
USER <USER@MSI>
2019-10-04 16:01:10 +0800

    加入lombok.Data

--- Designpattern/src/solid/dip/People.java
+++ Designpattern/src/solid/dip/People.java
@@ -1,21 +1,16 @@
 package solid.dip;
 
+import lombok.Data;
+import solid.ocp.Circle;
+
+@Data
 public class People {
 	private Stuffer stuffer;
 	
 	public People() {
 	}
 	
-	public Stuffer getStuffer() {
-		return stuffer;
-	}
-
-	public void setStuffer(Stuffer stuffer) {
-		this.stuffer = stuffer;
-	}
-
 	public void eat() {
-		// 填飽肚子
 		stuffer.stuff();
 	}
 }
